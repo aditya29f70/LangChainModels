f2424b37c373a5973a2e8e0d02aa5a2c0e026e83
Aditya Kumar <adityakumar29f70@gmail.com>
2025-10-08 02:55:26 +0530

    learnt about structured output -> pydantic

--- 5_structuredOutput/with_structured_output_typedDict.py
+++ 5_structuredOutput/with_structured_output_typedDict.py
@@ -1,6 +1,6 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
-from typing import TypedDict, Annotated
+from typing import TypedDict, Annotated, Optional, Literal
 
 load_dotenv()
 
@@ -18,8 +18,13 @@ class Review(TypedDict):
   # sentiment:str
   # instead to give only shema type now you can also give description
 
+  key_themes: Annotated[list[str], 'Write down all the key themes discussed in the review in a list']
   summary: Annotated[str, 'A brief summary of the review!']
-  sentiment: Annotated[str, 'Return sentiment of the review either negative, positive or neutral']
+  sentiment: Annotated[Literal['Pos', 'neg'], 'Return sentiment of the review either negative, positive or neutral']
+  pros: Annotated[Optional[list[str]], 'Write down all the pros inside a list']
+  cons: Annotated[Optional[list[str]], 'Write down all the cons inside a list']
+  name: Annotated[Optional[str], 'Write the name of reviewer']
+
 
 # now called to with_structured_output() fn on on model with 
 # what would be structure of output 'Review'
@@ -28,9 +33,9 @@ structured_model= model.with_structured_output(Review)
 # now this model has that schema 'Review' defination that how would our outcome structe looks like 
 # now instead of invokeing model now invoke this modeifed model
 
-result= structured_model.invoke("""The hardware is great, but the software feels bloated. There are too many pre- intalled apps that i can't remove. Also, the UI looks outdated compared to other brand, Hoping for a softare update to fix this""")
+# result= structured_model.invoke("""The hardware is great, but the software feels bloated. There are too many pre- intalled apps that i can't remove. Also, the UI looks outdated compared to other brand, Hoping for a softare update to fix this""")
 
-print(result)
+# print(result)
 
 
 # what is happening here ; like i am only giving that a Review class obj
@@ -62,7 +67,43 @@ return the response in JSON format
 # and also if there is not any pros or cons then don't give it ( mean if want to take this as a option things)
 
 # step-1 copy you big review that you had and put it at model.invoke('text_review'<--here)
-# 
+# step2 --> add one more shema attribute -> key_themes -> would we list bz multiple thems possible
+
+# step3 --> add proms shema in typedDict --> option--> so for this we use Optional module from typing
+
+# step4 --> lly cons --> option
+
+# if i want that in 'sentimate' response would give post or neg instead of full ward --> for this you have  'Literal' obj in typing
 
 
-# at 30: min
+
+
+result= structured_model.invoke(""" I’ve been using LearnSphere, an online learning platform, for nearly six months now to upskill in data analytics and AI. At first glance, the website looks sleek, and the onboarding process is simple — you can create an account and start exploring free content instantly. What drew me to LearnSphere was its promise of “personalized learning powered by AI.” In practice, this feature is quite impressive. The platform tracks your progress and weak areas, recommending new lessons or exercises tailored to your performance. The video quality is excellent, instructors are generally knowledgeable, and most courses include real-world projects that help reinforce learning.
+
+That said, not everything is perfect. The discussion forums, which are supposed to encourage peer interaction, are often inactive or filled with repetitive questions that go unanswered. Customer support also takes too long to respond — sometimes over 48 hours — which can be frustrating if you’re stuck on an assignment. Additionally, the pricing model feels a bit confusing. There are multiple subscription tiers, and the differences between them aren’t well explained. While the platform claims to offer job placement assistance, the support is limited to resume templates and a few webinars.
+
+Despite those downsides, I’ve noticed genuine improvement in my skills and confidence. The quizzes and mini-projects are thoughtfully designed, and the instructors’ feedback is often detailed. The AI mentor that suggests topics and study reminders is a nice touch, though it sometimes sends too many notifications. Overall, I think LearnSphere is an excellent tool for self-motivated learners who prefer flexibility and guided structure but don’t rely heavily on community support.""")
+
+print(result)
+print(result['summary'])
+print(result['sentiment'])
+
+
+
+
+# this works good --> but there is only one problem --> there is not any gerently that what ever you have
+# design you shema like somethings would be in 'list' or 'str' will be come as you have metioned
+# there is positivility that you want 'str' it is giving you list -->so there is posibility that 
+# your llm model can be wrong
+
+## finally mean you can use '''datavalidation'' here ; so typedDict only for represention learning porpush
+############## so for datavalidation you have another shema designer ==> called '''pydantic'''
+
+
+
+
+
+ 
+
+
+
